c4ac6790dd4c65f75f930659bfe113ebf594ff44
Abdullahi Ahmed <gw8332ck@go.minnstate.edu>
2025-11-23 17:34:00 -0600

    Completed pseudo code

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 """
-Full Name:
-Class-Section:
-Assignment Title:
-Submission Date:
+Full Name: Abdullahi Ahmed
+Class-Section: IS 250-01
+Assignment Title: Calculate Average of Three Scores
+Submission Date: 11/23/2025
 """
 """
-Write your pseudocode here.
-Do not write any Python code in this section.
-Your pseudocode should describe your overall approach in your own words.
+START
+ASK THE USER FOR THE FIRST EXAM SCORE
+STORE SCORE IN VARIABLE 
+
+ASK THE USER FOR THE SECOND EXAM SCORE
+STORE SCORE IN VARIABLE 
+
+ASK THE USER FOR THE THIRD EXAM SCORE
+STORE SCORE IN VARIABLE 
+
+DEFINE FUNCTION calculate_average(s1,s2,s3)
+    CALCULATE AVG: (s1 + s2 + s3) / 3
+    RETURN result 
+END FUNCTION 
+
+CALL calculate_average(s1,s2,s3)
+STORE result in average variable 
+
+PRINT the string "First score: ..." and score1
+PRINT the string "Second score: ..." and score2
+PRINT the string "Third score: ..." and score3
+PRINT the string "The average score: ..." and average
 """
 
 # Your Python code begins below this line.
+
 # Every line you write must have a comment directly above it.
 
 # Call your function when your program is ready
-calculate_average()   
+calculate_average()
   
